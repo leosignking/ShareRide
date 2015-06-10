215be0314ff1364557887c7f3c203bb896a82311
leosignking <redreddy.sridhar@gmail.com>
2015-06-10 19:50:22 -0400

    Updated core classes Added hibernate configuration

---
+++ src/main/java/com/shareride/profile/service/UserService.java
@@ -0,0 +1,47 @@
+package com.shareride.profile.service;
+
+import com.shareride.profile.beans.UserBean;
+import com.shareride.profile.dao.UserDao;
+import com.shareride.profile.model.Account;
+import com.shareride.profile.model.Profile;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @author sridhar.reddy
+ *
+ */
+
+@Service
+@Transactional(readOnly = true, propagation = Propagation.REQUIRED)
+public class UserService {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
+
+    @Autowired
+    private UserDao userDao;
+
+    public void createUser(UserBean userBean) {
+
+        Profile profile = new Profile();
+        profile.setFirstName(userBean.getFirstName());
+        profile.setLastName(userBean.getLastName());
+        profile.setMobileNo(userBean.getMobileNo());
+
+        Account account = new Account();
+        account.setEmail(userBean.getEmail());
+        account.setPassword(userBean.getPassword());
+        account.setProfile(profile);
+
+        userDao.saveUser(profile, account);
+
+    }
+
+
+
+
+}
