6f41b93cbd9f095ae4a0ca1d5c50ecf5b197d7e7
vanamsandeep <sandeepvanam58@gmail.com>
2015-06-16 01:44:17 -0400

    Added method for Login page validations

--- src/main/java/com/shareride/profile/controller/LoginController.java
+++ src/main/java/com/shareride/profile/controller/LoginController.java
@@ -3,9 +3,12 @@ package com.shareride.profile.controller;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.shareride.profile.beans.UserBean;
 
 
 @Controller
@@ -13,10 +16,11 @@ public class LoginController {
 
     private static final String indexViewName = "index";
     private static final String signupViewName = "signup";
+    private static final String dashboardViewName = "dashboard";
     private final Log logger = LogFactory.getLog(getClass());
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public ModelAndView indexPage(ModelAndView model) {
+    public ModelAndView indexPage(ModelAndView model, @ModelAttribute("userBean") UserBean userBean) {
 
         logger.info("Index Controller is called");
         model.setViewName(indexViewName);
@@ -30,5 +34,14 @@ public class LoginController {
         model.setViewName(signupViewName);
         return model;
     }
+    
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public ModelAndView loginPage(ModelAndView model, @ModelAttribute("userBean") UserBean userBean) {
 
+        logger.info("Index Controller loginPage is called");
+        System.out.println("User name/email is "+userBean.getEmail());
+        System.out.println("User name/email is "+userBean.getPassword());
+        model.setViewName(dashboardViewName);
+        return model;
+    }
 }
